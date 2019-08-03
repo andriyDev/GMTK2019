7890942bd18a6ef2fb9ae70374add1b548950349
fowlerbr <brennan.fowler@mail.utoronto.ca>
2019-08-03 16:36:07 -0400

    Add enemy movement and rotation toward player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class EnemyController : Attackable
 {
+    Rigidbody2D rb;
+    public PlayerController player;
+    public float MAX_SPEED = 2.0f;
+    public float ACCELERATION = 100.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Rotate()
+    {
+        Vector3 playerPosition = player.transform.position;
+        float angleRad = Mathf.Atan2(playerPosition.y - transform.position.y, playerPosition.x - transform.position.x);
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+        angleDeg -= 90;
+        transform.rotation = Quaternion.Euler(0, 0, angleDeg);
+    }
+
+    private void Move()
+    {
+        Vector3 newPosition = Vector3.Normalize(player.transform.position - transform.position);
+        rb.velocity = Vector3.MoveTowards(rb.velocity, newPosition * MAX_SPEED, ACCELERATION * Time.fixedDeltaTime);
+    }
+
     public override void onAttack() {
         Destroy(this.gameObject);
     }
