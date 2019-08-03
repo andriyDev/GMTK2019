b29b40a1aee28f869535f28aeb3f15305b2b784a
fowlerbr <brennan.fowler@mail.utoronto.ca>
2019-08-03 15:47:15 -0400

    Add collision and player rotation to mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
     public float speed = 10.0f;
+    public float MAX_SPEED = 9.0f;
+    public float ACCELERATION = 100.0f;
     List<Interactable> nearbyInteractables = new List<Interactable>();
     public AttackHandler attacker;
     public InteractHandler interacter;
@@ -30,14 +32,24 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Rotate();
     }
 
     private void Move()
     {
-        float translationY = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
-        float translationX = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        float translationY = Input.GetAxis("Vertical");
+        float translationX = Input.GetAxis("Horizontal");
+        rb.velocity = Vector3.MoveTowards(rb.velocity, new Vector3(translationX, translationY, 0) * MAX_SPEED, ACCELERATION * Time.fixedDeltaTime);
+    }
 
-        transform.Translate(translationX, translationY, 0);
+    private void Rotate()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, -5));
+        float angleRad = Mathf.Atan2(mouseWorld.y - transform.position.y, mouseWorld.x - transform.position.x);
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+        angleDeg -= 90;
+        transform.rotation = Quaternion.Euler(0, 0, angleDeg);
     }
 
     private void Interact() {
