439333c41c6413d4f191da5ebb3dc5a4f44e8275
fowlerbr <brennan.fowler@mail.utoronto.ca>
2019-08-03 11:25:16 -0400

    Add player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float translationY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float translationX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(translationX, translationY, 0);
     }
 }
