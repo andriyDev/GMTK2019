d8262d410a1c6c960ca61931f67d03bc5bdfe781
Andriy Dzikh <andriydzikh@gmail.com>
2019-08-03 13:05:46 -0400

    Turned direction into a get-set for LightSource. Added a lightOn variable which is a get-set to LightSource.

--- Assets/Scripts/LightSource.cs
+++ Assets/Scripts/LightSource.cs
@@ -4,14 +4,67 @@ using UnityEngine;
 
 public class LightSource : MonoBehaviour
 {
-    public int direction;
+    [SerializeField]
+    public bool lightOn
+    {
+        get
+        {
+            return _lightOn;
+        }
+        set
+        {
+            _lightOn = value;
+            foreach (Lightable lightable in lightables)
+            {
+                if (_lightOn)
+                {
+                    lightable.Light(direction);
+                }
+                else
+                {
+                    lightable.Unlight(direction);
+                }
+            }
+        }
+    }
+
+    public int direction
+    {
+        get
+        {
+            return _direction;
+        }
+        set
+        {
+            foreach (Lightable lightable in lightables)
+            {
+                lightable.Unlight(direction);
+            }
+            _direction = value;
+            foreach(Lightable lightable in lightables)
+            {
+                lightable.Light(direction);
+            }
+        }
+    }
+
+    [SerializeField]
+    private bool _lightOn;
+    [SerializeField]
+    private int _direction;
+
+    private List<Lightable> lightables = new List<Lightable>();
 
     void OnTriggerEnter2D(Collider2D c)
     {
         Lightable l = c.gameObject.GetComponent<Lightable>();
         if(l)
         {
-            l.Light(direction);
+            lightables.Add(l);
+            if(lightOn)
+            {
+                l.Light(direction);
+            }
         }
     }
 
@@ -20,7 +73,11 @@ public class LightSource : MonoBehaviour
         Lightable l = c.gameObject.GetComponent<Lightable>();
         if(l)
         {
-            l.Unlight(direction);
+            lightables.Remove(l);
+            if(!lightOn)
+            {
+                l.Unlight(direction);
+            }
         }
     }
 
