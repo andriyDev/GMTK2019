608fd80ed2a2259cd3aeecbd7469b26072749dba
fowlerbr <brennan.fowler@mail.utoronto.ca>
2019-08-03 13:09:59 -0400

    Add enemy and attack

---
+++ Assets/Scripts/AttackHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHandler : MonoBehaviour
+{
+    List<Attackable> _nearbyAttackables = new List<Attackable>();
+
+    public List<Attackable> nearbyAttackables
+    {
+        get {
+            return _nearbyAttackables;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Attackable other = otherCollider.gameObject.GetComponent<Attackable>();
+        if (other != null) {
+            _nearbyAttackables.Add(other);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        Attackable other = otherCollider.gameObject.GetComponent<Attackable>();
+        if (other != null) {
+            _nearbyAttackables.Remove(other);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Attackable
+{
+    public override void onAttack() {
+        Destroy(this.gameObject);
+    }
+}
